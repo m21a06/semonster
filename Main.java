a51be5c895121275024933bc11ac762bcde0aea4
m21a06 <m1m21a06@st.oit.ac.jp>
2021-06-28 14:44:52 +0900

    add semonster main class

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("俺はいそべ");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
